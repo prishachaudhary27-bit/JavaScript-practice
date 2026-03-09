597fc2ca4ae92b896fdb541319b6174cea5b2fe4
prishachaudhary27-bit <prishachaudhary27@gmail.com>
2026-03-10 04:07:09 +0530

    used if loop to compare 2 numbers

---
+++ script.js
@@ -0,0 +1,10 @@
+let a=23
+let b=prompt("enter a number")
+b=Number.parseInt(b);
+if(a>b){
+    console.log("a is greater than b")
+}else if(a==b){
+    console.log("a is equal to b")
+}else{
+    console.log("a is less than b")
+}
